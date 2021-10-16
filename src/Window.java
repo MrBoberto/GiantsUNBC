8f0999598558a7f992db8049f77d0127bc1de89c
AbhiArora1998 <90983067+AbhiArora1998@users.noreply.github.com>
2021-10-16 10:54:27 -0700

    window.add

---
+++ src/Window.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Window extends JFrame {
+    public Window(int width, int height){
+        setTitle("Your Mom");// title of the main window
+        setResizable(false);
+        getContentPane().setPreferredSize(new Dimension(width, height));// set the size of the window
+        pack();
+
+        setLocationRelativeTo(null);// puts the window in the middle of the screen
+        setVisible(true);// to make our window visible
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// close the program when the window is close
+    }
+}
