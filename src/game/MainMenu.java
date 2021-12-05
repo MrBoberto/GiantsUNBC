1116e1fe43d6f05e73de628e8839c661b8e6f2ae
Noah Stobbe <stobbe@unbc.ca>
2021-12-04 20:38:46 -0800

    Added Clock

--- src/game/MainMenu.java
+++ src/game/MainMenu.java
@@ -753,8 +753,25 @@ public class MainMenu implements KeyListener {
         }
     }
 
-    static class Timer {
+    static class Clock {
+        private static int time =0;
 
+        private Clock(){}//Empty
+
+        /**
+         * This is incrementing everyclock cycle
+         */
+        public static void advanceClock(){
+            time++;
+        }
+
+        /**
+         * provides  us the current clock time
+         * @return the time variable whenever called
+         */
+        public static int getCurrentClockTime(){
+            return time;
+        }
     }
 }
 
